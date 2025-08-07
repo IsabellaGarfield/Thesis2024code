7b54de61ef81d02edc79e82375f5014d43c8d939
Max Czapanskiy <47009796+FlukeAndFeather@users.noreply.github.com>
2025-08-07 11:43:31 -0700

    Gamma and quantile regression

--- code/lags.R
+++ code/lags.R
@@ -1,9 +1,10 @@
 library(tidyverse)
-
-#calculating lags from resights
+library(glmmTMB)
+library(boot)
 
 pups_moms <- read_rds("output/pups_moms.rds")
-beach_distances <- read_rds("output/beach_distance.RDS")
+beach_distances <- read_rds("output/beach_distance.RDS") %>% 
+  mutate(Distance = as.numeric(Distance, unit = "m"))
 
 lags_distances <- pups_moms %>% 
   group_by(momID) %>% 
@@ -13,19 +14,64 @@ lags_distances <- pups_moms %>%
       mutate(lag = season.y - season.x) %>% 
       select(pup_area.x, pup_area.y, lag)
   }) %>% 
-  left_join(beach_distances, by = c(pup_area.x = "mom_area", pup_area.y = "pup_area"))
+  ungroup() %>% 
+  left_join(beach_distances, 
+            by = c(pup_area.x = "mom_area", pup_area.y = "pup_area")) %>% 
+  drop_na(Distance)
 
 ggplot(lags_distances, aes(factor(lag), Distance)) +
-  geom_boxplot()
+  geom_boxplot() +
+  theme_bw()
 
-neg_log_lik <- function(beta, X, y) {
-  mu <- X %*% beta
-  if (any(mu <= 0)) return(Inf)  # penalize invalid values
-  nll <- -sum(dgamma(y, shape = 1/phi, scale = mu * phi, log = TRUE))  # phi = dispersion
-  return(nll)
-}
+# Some of your Distances are 0's, meaning gamma won't work
+# Trying zero-inflated gamma, using glmmTMB package
 
-lag_mod <- constrOptim(
-  theta = c(1, 1),
-  f = neg_log_lik,
-  grad = NULL
+model_lags <- glmmTMB(Distance ~ log(lag) + (1 | momID),
+                      ziformula = ~ lag + (1 | momID),
+                      data = lags_distances,
+                      family = ziGamma(link = "identity"))
+summary(model_lags)
+
+# Model predictions 
+predictions_lags <- tibble(lag = 1:6) %>% 
+  mutate(Distance = predict(model_lags, 
+                            newdata = ., 
+                            type = "response",
+                            re.form = NA))
+
+# Visualize
+mean_distances <- lags_distances %>% 
+  group_by(lag) %>% 
+  summarise(Distance = mean(Distance))
+ggplot(lags_distances, aes(factor(lag), Distance)) +
+  geom_boxplot() +
+  geom_line(aes(group = 1),
+            data = predictions_lags, 
+            color = "cornflowerblue", 
+            linewidth = 1) +
+  geom_point(data = mean_distances,
+             color = "firebrick",
+             size = 2) +
+  theme_bw()
+
+# The lag does not have a significant effect on the distance. This is largely because the mean (red dots) doesn't change very much! The median does seem to change a lot though. You can investigate that with quantile regression. 
+
+library(quantreg)
+model_lags_quantile <- rq(Distance ~ log(lag), 
+                          tau = 0.5,
+                          data = lags_distances)
+summary(model_lags_quantile, se = "boot")
+quant_preds <- tibble(lag = 1:6) %>% 
+  mutate(Distance = predict(model_lags_quantile, newdata = .))
+ggplot(lags_distances, aes(factor(lag), Distance)) +
+  geom_boxplot() +
+  geom_line(aes(group = 1),
+            data = quant_preds, 
+            color = "cornflowerblue", 
+            linewidth = 1) +
+  geom_point(data = mean_distances,
+             color = "firebrick",
+             size = 2) +
+  theme_bw()
+
+## This model suggests the median distance does increase with the lag. You don't have a significant p-value (0.063), so you can't say it's a significant relationship. But you can present the results and that will satisfy the reviewer.
